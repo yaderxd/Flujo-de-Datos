104bd6a6f5099b302ed86211fddc920827d23ec5
yaderxd <yaderoviedo4@gmail.com>
2022-04-07 13:28:02 -0600

    cambio

---
+++ Domain/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private string cedula;
+
+        //public Persona(string nombre, string apellido, int edad, string cedula)
+        //{
+        //    this.Nombre = nombre;
+        //    this.Apellido = apellido;
+        //    this.Edad = edad;
+        //    this.Cedula = cedula;
+        //}
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Cedula { get => cedula; set => cedula = value; }
+
+        Persona[] GrupoPersona =
+        {
+            new Persona(){Nombre="Cesar",Apellido="Jafet",Edad=18,Cedula="1265664435"},
+             new Persona(){Nombre="yader",Apellido="Mendez",Edad=19,Cedula="126566435435"}
+        };
+    }
+}
--- Flujo de Datos/Program.cs
+++ Flujo de Datos/Program.cs
@@ -8,10 +8,26 @@ namespace Flujo_de_Datos
     {
         static void Main(string[] args)
         {
-            using (FileStream  fs= new FileStream("Prueba.txt",FileMode.Create,FileAccess.ReadWrite))
+            //using (FileStream  fs= new FileStream("Prueba.txt",FileMode.Create,FileAccess.ReadWrite))
+            //{
+            //    using (StreamWriter sw = new StreamWriter(fs))
+            //    {
+            //        sw.WriteLine("Hey bro,Nice Dick");
+            //        sw.Write("IP:105.03.236");
+            //        sw.Write("Diegp es Gay");
+            //    }
+            //    //using (StreamReader sr = new StreamReader(fs))
+            //    //{
+            //    //    sr.ReadToEnd();
+            //    //}
+            //}
+            using (FileStream fs = new FileStream("Prueba.txt", FileMode.Open, FileAccess.ReadWrite))
             {
-
-            } 
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
         }
     }
 }

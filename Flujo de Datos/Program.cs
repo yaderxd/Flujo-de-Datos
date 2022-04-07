886b68a18b02911d88a00eabd5c126f5ea153fa2
yaderxd <yaderoviedo4@gmail.com>
2022-04-07 13:25:19 -0600

    cambio

---
+++ Flujo de Datos/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.IO;
+
+namespace Flujo_de_Datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream  fs= new FileStream("Prueba.txt",FileMode.Create,FileAccess.ReadWrite))
+            {
+
+            } 
+        }
+    }
+}
